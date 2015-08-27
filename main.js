c2b2f76ef065a1cc90bf3668148b30f2eb5795d2
whitfieldc <whitfield.cw@gmail.com>
2015-08-26 22:22:37 -0700

    begin rethinkdb setup

--- main.js
+++ main.js
@@ -3,11 +3,33 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//load config for rethink
 var config = require('./config.js');
 
 var r = require('rethinkdb');
 
+var port = Number(process.env.PORT || 3000)
+
+var startApp = function(){
+    http.listen(port, function(){
+    console.log('listening on 3000');
+    });
+}
+
+var createConnection = function(req, res, next){
+    r.connect(config.rethinkdb).then(function(conn){
+        req._rdbConn = conn;
+        next();
+    }).error(handleError(res));
+}
+
+var closeConnection = function(req, res, next){
+    req._rdbConn.close();
+}
 app.use(express.static('public'));
+
+//create conn to DB
+app.use(createConnection);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -29,8 +51,42 @@ io.on('connection', function(socket){
     })
 });
 
-var port = Number(process.env.PORT || 3000)
+app.use(closeConnection);
 
-http.listen(port, function(){
-    console.log('listening on 3000');
+
+r.connect(config.rethinkdb, function(err, conn){
+    if (err){
+        console.log("Couldn't open connection to initialize DB:");
+        console.log(err.message);
+        process.exit(1);
+    }
+
+    r.table('seats').indexWait('createdAt').run(conn).then(function(err, result){
+        console.log("Table & index are available...");
+        startApp();
+    }).error(function(err){
+        r.dbCreate(config.rethinkdb.db).run(conn).finally(function(){
+            r.table('seats').indexCreate('createdAt').run(conn);
+        }).finally(function(){
+            r.table('seats').indexCreate('createdAt').run(conn);
+        }).finally(function(result){
+            r.table('seats').indexWait('createdAt').run(conn);
+        }).then(function(result){
+            console.log("Table & index are available...");
+            startApp();
+            conn.close();
+        }).error(function(err){
+            if (err){
+                console.log("Couldn't wait for completion of index");
+                console.log(err);
+                process.exit(1);
+            }
+            console.log("Table & index are available...");
+            startApp();
+            conn.close();
+        });
+    });
 });
+
+
+
