4e2558d51e6b74f4ea3732d768ec5a0feb41abf1
whitfieldc <whitfield.cw@gmail.com>
2015-09-03 23:34:01 -0700

    add working rethinkdb persistence layer

--- server.js
+++ server.js
@@ -2,24 +2,11 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-//load config for rethink
-var config = require('./config.js');
-
-var r = require('rethinkdb');
-require('rethinkdb-init')(r);
+var r = require('./db.js');
 
 var port = Number(process.env.PORT || 3000);
-
-r.init(config.rethinkdb, [
-  {
-    name: 'seats',
-    indexes: ['created']
-  }
-]).then(function(conn){
-  r.conn = conn;
-  r.conn.use((config.rethinkdb).db)
-});
+//load config for rethink
+var config = require('./config.js');
 
 http.listen(port);
 
@@ -28,20 +15,20 @@ app.get('/', function(req, res){
 });
 app.use(express.static('public'));
 
-io.on('connection', function(socket){
+r.connect(config.rethinkdb)
+.then(function(conn){
+  io.on('connection', function(socket){
 
-  r.table('seats')
-  .orderBy({index:'created'})
-  .coerceTo('array')
-  .run(r.conn)
-  .then(function(seats){
-    seats.forEach(function(seat){
-      socket.emit('seat', seat);
+    r.table('seats')
+    .orderBy({index:'created'})
+    .coerceTo('array')
+    .run(conn)
+    .then(function(seats){
+      seats.forEach(function(seat){
+        socket.emit('seat', seat);
+      });
     });
-  });
 
-  r.connect(config.rethinkdb)
-  .then(function(conn){
     r.table('seats')
     .changes().run(conn)
     .then(function(cursor){
@@ -51,15 +38,15 @@ io.on('connection', function(socket){
         console.log("finished");
       });
     });
-  });
 
-  socket.on('seat', function(data){
-    r.table('seats').insert({
-      note: data.note,
-      lat: data.lat,
-      lon: data.lon,
-      created: (new Date()).getTime()
-    }).run(r.conn);
-  });
+    socket.on('seat', function(data){
+      r.table('seats').insert({
+        note: data.note,
+        lat: data.lat,
+        lon: data.lon,
+        created: (new Date()).getTime()
+      }).run(conn);
+    });
 
+  });
 });
